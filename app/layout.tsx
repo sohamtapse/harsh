f138dbd5ef56580853bd07d9d914f6f86a05bee1
sohamtapse <tapsesoham0@gmail.com>
2025-11-01 12:40:56 +0530

    css

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-gray-800 h-full">{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,207 +3,209 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-      <div className="p-5 w-full max-w-6xl mx-auto">
-        <h1 className="text-3xl font-semibold mb-4 text-gray-800 text-center md:text-left">
-          Java
-        </h1>
-        <div className="flex flex-wrap justify-center md:justify-start gap-3">
-          <a
-            href="/JAVA/Prac1.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac1.txt
-          </a>
-          <a
-            href="/JAVA/Prac2.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac2.txt
-          </a>
-          <a
-            href="/JAVA/Prac3.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac3.txt
-          </a>
-          <a
-            href="/JAVA/Prac4.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac4.txt
-          </a>
-          <a
-            href="/JAVA/Prac5.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac5.txt
-          </a>
-          <a
-            href="/JAVA/Prac6.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac6.txt
-          </a>
-          <a
-            href="/JAVA/Prac7.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac7.txt
-          </a>
-          <a
-            href="/JAVA/Prac8.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac8.txt
-          </a>
-          <a
-            href="/JAVA/Prac9.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac9.txt
-          </a>
-          <a
-            href="/JAVA/Prac10.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac10.txt
-          </a>
-          <a
-            href="/JAVA/Prac11.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac11.txt
-          </a>
-          <a
-            href="/JAVA/Prac12.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac12.txt
-          </a>
+      <div className="bg-gray-800 h-full ">
+        <div className="p-5 w-full max-w-6xl mx-auto bg-gray-800">
+          <h1 className="text-3xl font-semibold mb-4 text-white text-center md:text-left">
+            Java
+          </h1>
+          <div className="flex flex-wrap justify-center md:justify-start gap-3">
+            <a
+              href="/JAVA/Prac1.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac1.txt
+            </a>
+            <a
+              href="/JAVA/Prac2.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac2.txt
+            </a>
+            <a
+              href="/JAVA/Prac3.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac3.txt
+            </a>
+            <a
+              href="/JAVA/Prac4.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac4.txt
+            </a>
+            <a
+              href="/JAVA/Prac5.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac5.txt
+            </a>
+            <a
+              href="/JAVA/Prac6.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac6.txt
+            </a>
+            <a
+              href="/JAVA/Prac7.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac7.txt
+            </a>
+            <a
+              href="/JAVA/Prac8.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac8.txt
+            </a>
+            <a
+              href="/JAVA/Prac9.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac9.txt
+            </a>
+            <a
+              href="/JAVA/Prac10.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac10.txt
+            </a>
+            <a
+              href="/JAVA/Prac11.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac11.txt
+            </a>
+            <a
+              href="/JAVA/Prac12.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac12.txt
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="p-5 w-full max-w-6xl mx-auto">
-        <h1 className="text-3xl font-semibold mb-4 text-gray-800 text-center md:text-left">
-          DC
-        </h1>
-        <div className="flex flex-wrap justify-center md:justify-start gap-3">
-          <a
-            href="/DC/Prac5.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac5.txt
-          </a>
-          <a
-            href="/DC/Prac6.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac6.txt
-          </a>
-          <a
-            href="/DC/Prac7.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac7.txt
-          </a>
-          <a
-            href="/DC/Prac8.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac8.txt
-          </a>
-          <a
-            href="/DC/Prac9.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac9.txt
-          </a>
-          <a
-            href="/DC/Prac10.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac10.txt
-          </a>
+        <div className="p-5 w-full max-w-6xl mx-auto bg-gray-800">
+          <h1 className="text-3xl font-semibold mb-4  text-white text-center md:text-left">
+            DC
+          </h1>
+          <div className="flex flex-wrap justify-center md:justify-start gap-3">
+            <a
+              href="/DC/Prac5.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac5.txt
+            </a>
+            <a
+              href="/DC/Prac6.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac6.txt
+            </a>
+            <a
+              href="/DC/Prac7.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac7.txt
+            </a>
+            <a
+              href="/DC/Prac8.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac8.txt
+            </a>
+            <a
+              href="/DC/Prac9.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac9.txt
+            </a>
+            <a
+              href="/DC/Prac10.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac10.txt
+            </a>
+          </div>
         </div>
-      </div>
-      <div className="p-5 w-full max-w-6xl mx-auto">
-        <h1 className="text-3xl font-semibold mb-4 text-gray-800 text-center md:text-left">
-          MC
-        </h1>
-        <div className="flex flex-wrap justify-center md:justify-start gap-3">
-          <a
-            href="/MC/Prac1.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac1.txt
-          </a>
-          <a
-            href="/MC/Prac2.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac2.txt
-          </a>
-          <a
-            href="/MC/Prac3.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac3.txt
-          </a>
-          <a
-            href="/MC/Prac4.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac4.txt
-          </a>
-          <a
-            href="/MC/Prac5.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac5.txt
-          </a>
-          <a
-            href="/MC/Prac6.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac6.txt
-          </a>
-          <a
-            href="/MC/Prac7.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac7.txt
-          </a>
-          <a
-            href="/MC/Prac8.txt"
-            download
-            className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
-          >
-            Prac8.txt
-          </a>
+        <div className="p-5 w-full h-full max-w-6xl mx-auto  bg-gray-800">
+          <h1 className="text-3xl font-semibold mb-4  text-white text-center md:text-left">
+            MC
+          </h1>
+          <div className="flex flex-wrap justify-center md:justify-start gap-3">
+            <a
+              href="/MC/Prac1.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac1.txt
+            </a>
+            <a
+              href="/MC/Prac2.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac2.txt
+            </a>
+            <a
+              href="/MC/Prac3.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac3.txt
+            </a>
+            <a
+              href="/MC/Prac4.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac4.txt
+            </a>
+            <a
+              href="/MC/Prac5.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac5.txt
+            </a>
+            <a
+              href="/MC/Prac6.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac6.txt
+            </a>
+            <a
+              href="/MC/Prac7.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac7.txt
+            </a>
+            <a
+              href="/MC/Prac8.txt"
+              download
+              className="px-6 py-3 bg-black text-white hover:bg-white hover:text-black rounded shadow-md transition w-full sm:w-auto text-center"
+            >
+              Prac8.txt
+            </a>
+          </div>
         </div>
       </div>
     </>
